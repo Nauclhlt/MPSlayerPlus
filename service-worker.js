e28b6b0439f01674d6fe1d78952d0c3f9197f38d
Nauclhlt <rei.20080312@gmail.com>
2025-08-26 20:27:08 +0900

    initial commit.

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "script-viewer-v1";
+const ASSETS = [
+  "index.html",
+  "styles.css",
+  "app.js",
+  "script.txt",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  "audio/explode.mp3"
+  // 必要に応じて audio/ 内のファイルも追加
+];
+
+// インストール時にキャッシュ
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// オフライン対応
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
